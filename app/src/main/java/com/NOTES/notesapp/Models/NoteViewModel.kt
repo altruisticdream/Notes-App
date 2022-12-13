90c8c68d19c760cb446b6e8882d8cf8957c50f22
altruisticdream <ashutoshgautam9628@outlook.com>
2022-12-14 04:01:45 +0530

    First Repo

---
+++ app/src/main/java/com/NOTES/notesapp/Models/NoteViewModel.kt
@@ -0,0 +1,29 @@
+package com.NOTES.notesapp.Models
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.NOTES.notesapp.Database.NoteDatabase
+import com.NOTES.notesapp.Database.NotesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository : NotesRepository
+    val allnotes : LiveData<List<Note>>
+    init{
+        val dao=NoteDatabase.getDatabase(application).getNoteDao()
+        repository= NotesRepository(dao)
+        allnotes=repository.allNotes
+    }
+    fun deleteNote(note:Note)=viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+    fun insertNote(note: Note)=viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+    fun updateNote(note: Note)=viewModelScope.launch(Dispatchers.IO){
+        repository.update(note)
+    }
+}
---
+++ app/src/main/java/com/NOTES/notesapp/Utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.NOTES.notesapp.Utilities
+
+const val DATABASE_NAME="note_database"
